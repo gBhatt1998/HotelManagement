63e1464cb1ba60fe6d4e20c0ccb302b21794d658
gaurav kumar bhatt <gauravbhatt1998@gmail.com>
2025-06-13 01:30:19 +0530

    refactored available room and by date to resutful api rules

--- src/app/reservation/roomlist.service.ts
+++ src/app/reservation/roomlist.service.ts
@@ -1,23 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Room } from './models/room.model';
+import { ApiResponse } from '../shared/models/api-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomlistService {
 
-  private baseUrl = 'http://localhost:8080/reservation'; 
+  private baseUrl = 'http://localhost:8080/room-types/available'; 
   constructor(private http: HttpClient) {}
 
   getAllAvailableRooms(): Observable<Room[]> {
-    return this.http.get<Room[]>(`${this.baseUrl}/rooms/available/types`);
+    return this.http.get<ApiResponse<Room[]>>(`${this.baseUrl}`).pipe(
+      map(response => {
+        if (!response.data || !Array.isArray(response.data)) {
+          throw new Error('Invalid room data from server.');
+        }
+        return response.data;
+      })
+    );
   }
 
   getAvailableRoomsByDate(checkIn: string, checkOut: string): Observable<Room[]> {
-    return this.http.get<Room[]>(
-      `${this.baseUrl}/rooms/available/by-date?checkIn=${checkIn}&checkOut=${checkOut}`
-    );
+    return this.http
+      .get<ApiResponse<Room[]>>(
+        `${this.baseUrl}/by-date?checkIn=${checkIn}&checkOut=${checkOut}`
+      )
+      .pipe(
+        map(response => {
+          if (!response.data || !Array.isArray(response.data)) {
+            throw new Error('Invalid room data from server.');
+          }
+          return response.data;
+        })
+      );
   }
 }
